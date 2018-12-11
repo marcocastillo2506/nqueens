82e2bd05628d09e51869f10f78f5083b4d4622e1
seisvelas <alej.alvarado94@gmail.com>
2018-12-11 00:33:40 -0600

    Made everything ormy

--- nqueens.py
+++ nqueens.py
@@ -1,62 +1,75 @@
 from sqlalchemy import *  ## Our poor, poor global namespace
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-## nqueens 'public' methods: solve(n=8), psql_commit(user, passw, db, port)
-class nqueens:
-    n = None
-    
-    def __init__(self, n=None):
-        if n is not None:
-            self.n = n
-        
-    def threatened(self, pos, past, n):
-        for i in range(len(past)):
-            if past[i] == pos or abs(pos - past[i]) == len(past) - i:
-                return True
-        return False
 
-    def nqueens_gen(self, n, past=None):
-        if past == None:
-            for i in range(n):
-                yield from self.nqueens_gen(n, [i+1])
+PSQL_USER = 'postgres'
+PSQL_PASS = ''
+PSQL_DB   = 'db'
+PSQL_PORT = 5432
+
+engine = create_engine('postgresql://%s:%s@localhost:%i/%s' %
+                               (PSQL_USER, PSQL_PASS, PSQL_PORT, PSQL_DB))
+
+Base = declarative_base()
+
+class Solution(Base):
+    __tablename__ = 'solutions'
+
+    id = Column(Integer, primary_key=True)
+    n = Column(Integer)
+    sol = Column(VARCHAR(512))
+
+    def __repr__(self):
+        return "<User(n='%s', sol='%s')>" % (
+                             self.n, self.sol)
+
+
+def threatened(pos, past, n):
+    for i in range(len(past)):
+        if past[i] == pos or abs(pos - past[i]) == len(past) - i:
+            return True
+    return False
+
+
+def nqueens(n, past=None):
+    if past == None:
+        for i in range(n):
+            yield from nqueens(n, [i+1])
+    else:
+        if len(past) == n:
+            yield past[:]
         else:
-            if len(past) == n:
-                yield past[:]
-            else:
-                for pos in range(1,n+1):
-                    if not self.threatened(pos, past, n):
-                        yield from self.nqueens_gen(n,past + [pos])
+            for pos in range(1,n+1):
+                if not threatened(pos, past, n):
+                    yield from nqueens(n,past + [pos])
 
-    def solve(self, n=8):
-        self.n = n
-        self.nqg = self.nqueens_gen(n)
-        return self.nqg
 
-    def psql_commit(self, user="postgres", passw="", db="", port=5432):
-        assert self.nqg is not None # must call self.solve() first.
-        engine = create_engine('postgresql://%s:%s@localhost:%i/%s' %
-                               (user, passw, port, db))
+def psql_insert_queens(n, queen_generator):
+    Base.metadata.create_all(engine)
 
-        ## Create solutions table if not exists
-        
-        meta = MetaData()
-        solutions = Table("solutions", meta,
-                        Column("n", Integer),
-                        Column("sol", String(512)))
-        
-        if not engine.dialect.has_table(engine, "solutions"):
-            solutions.create(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
-        for solution in self.nqg:
-            ins = solutions.insert().values(n=self.n, sol=",".join([str(i) for i in solution]))
-            engine.execute(ins)            
+    for sol in queen_generator:
+        solution = Solution(n=n, sol=",".join(map(str, sol)))
+        session.add(solution)
+
+    session.commit()
+
+    return session
+
 
 ##
 #### Example: Insert all 8 queens solutions into db:
+## session.query(Solution).filter_by(n=12).count
 ##
-## nq = nqueens()
+## n = 8
+## queen_gen = nqueens(n)
+## psql_insert_queens(n, queen_gen)
 ##
-## nq.solve(8) # returns a generator of solutions, also saves generator of n queens inside object
-##             # that can then be committed to db
-##            
-## nq.psql_commit("postgres", "fl33021", "queens")
+##
+##  Now, psql_insert_queens returns the session object (which is also creates, which is bad design, but anyway)
+##  which is consequential, because it allows us to continue querying the db. This is great for our tests!
 
