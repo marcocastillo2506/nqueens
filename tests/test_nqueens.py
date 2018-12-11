a862b6f4a426c7e1d9958e5b1b35750ea973481e
seisvelas <alej.alvarado94@gmail.com>
2018-12-11 01:54:30 -0600

    Made everything ormy

--- tests/test_nqueens.py
+++ tests/test_nqueens.py
@@ -1,12 +1,14 @@
 from ..nqueens import nqueens, psql_insert_queens, Solution
-from .fixtures import OEIS_solution_sizes
+from .fixtures import sol_sizes
 
 
 def test_nq():
-    from_n = 9
+    from_n = 9  # test nqueens/psql_insert_queens for n=1,n=2,...,n=from_n
 
-    while from_n:
-        queen_gen = nqueens(from_n)
-        session = psql_insert_queens(from_n, queen_gen)
-        assert session.query(Solution).filter_by(n=from_n).count() == OEIS_solution_sizes[from_n-1]
-        from_n -= 1
+    # Below we abuse a passing knowledge of functional programming to
+    # convert what was once a few elegant, easily readible lines
+    # of straightforward, Pythonic imperative code into a hideous
+    # incomprehensible mess. Looking at the proceeding line reminds
+    # me of why Haskell and Lisp not only will not, but do not deserve,
+    # to ever become mainstream industrial programming languages.
+    assert False not in map(lambda x: psql_insert_queens(x, nqueens(x)).query(Solution).filter_by.count() == sol_sizes[x-1])
