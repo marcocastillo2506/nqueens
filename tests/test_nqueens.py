fa9567480b2f58a0c838d4c605b22a524a24308c
seisvelas <alej.alvarado94@gmail.com>
2018-12-11 01:20:14 -0600

    Made everything ormy

--- tests/test_nqueens.py
+++ tests/test_nqueens.py
@@ -3,10 +3,10 @@ from .fixtures import OEIS_solution_sizes
 
 
 def test_nq():
-    from_n = 8
+    from_n = 9
 
     while from_n:
         queen_gen = nqueens(from_n)
         session = psql_insert_queens(from_n, queen_gen)
-        assert session.query(Solution).filter_by(n=8).count() == OEIS_solution_sizes[7]
+        assert session.query(Solution).filter_by(n=from_n).count() == OEIS_solution_sizes[from_n-1]
         from_n -= 1
